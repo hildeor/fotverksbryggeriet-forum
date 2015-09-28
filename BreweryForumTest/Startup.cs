366dbb0a138c41470eb5a8f9c5f53e57bb745fb8
hildeor <hilde.reindal@gmail.com>
2015-09-28 15:08:17 +0200

    First commit

---
+++ BreweryForumTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BreweryForumTest.Startup))]
+namespace BreweryForumTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
